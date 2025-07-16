a4c030fc9f0e97c2a5f686b33db0d97ced4a7a71
Irfan <irfan.ik227@gmail.com>
2025-07-17 02:43:01 +0500

    minor updates

---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native";
+
+
+export default function HistoryPage() {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      {/* Your history page content goes here */}
+    </SafeAreaView>
+  );
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet } from 'react-native';
-import HomePage from './HomePage';
-
+import { SafeAreaView, StyleSheet } from "react-native";
+import HomePage from "./home";
 
 export default function HomeScreen() {
   return (
-    <HomePage />
+    <SafeAreaView style={{ flex: 1 }}>
+      <HomePage />
+    </SafeAreaView>
     // <ParallaxScrollView
     //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
     //   headerImage={
@@ -54,8 +55,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -67,6 +68,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
